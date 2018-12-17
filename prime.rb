1c9d8cae498b909039f3913274f56c52be92b0f5
Michael Soares <mikey.soares@gmail.com>
2018-12-17 15:45:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 # Add  code here!
 
 def prime?(n)
-return false if n < 2
-return true if n == 2
-(2..n - 1).each do |i|
-  return false if n / x == 0
+if n < 2
+  return false
+end
+
+(2..n).each do |i|
+  return false if n % i == 0
     end
     return true
 end
